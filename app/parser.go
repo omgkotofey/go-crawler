011d1839e7b8d973e809512ac9970b907c0f5244
Ilia Solovev <is@fv.ee>
2023-07-16 20:03:19 +0300

    Fixed bahaviour for relative links started with dot symbol. Removed unused files.

--- app/parser.go
+++ app/parser.go
@@ -17,6 +17,15 @@ func (o Origin) getBase() string {
 	return fmt.Sprintf("%v://%v", o.Base.Scheme, o.Base.Host)
 }
 
+func (o Origin) getAbsolute(relativeUrl string) string {
+	absoluteUrl, err := o.Base.Parse(relativeUrl)
+	if err != nil {
+		return ""
+	}
+
+	return absoluteUrl.String()
+}
+
 type ATag struct {
 	token  html.Token
 	origin Origin
@@ -30,16 +39,24 @@ func (t ATag) getHref() (href string, ok bool) {
 		}
 	}
 
+	// For http(s) urls
 	if strings.Index(href, "http") == 0 {
 		return href, true
 	}
 
+	// For "/" and "" shortcuts
 	if href == "/" || href == "" {
 		return t.origin.getBase(), true
 	}
 
+	// For relative urls starts with slash
 	if strings.Index(href, "/") == 0 {
 		return fmt.Sprintf("%v%v", t.origin.getBase(), href), true
+	}
+
+	// For relative urls starts with dot
+	if strings.Index(href, ".") == 0 {
+		return t.origin.getAbsolute(href), true
 	}
 
 	return href, false
